94423f269824ad405ddfba2ced3bff454f69e952
coderbennett <joeykb17@gmail.com>
2022-04-23 15:24:37 -0700

    add intern class test cases

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -2,28 +2,28 @@ const Employee = require("../lib/Employee");
 
 describe("Employee class", () => {
     describe("getName method", () => {
-        it("returns employee name", () => {
+        it("should return employee name", () => {
             const employee = new Employee("Edgar", 1012243, "edgar@greenleaf.com");
             expect(employee.getName().toBe("Edgar"));
         });
     });
 
     describe("getId method", () => {
-        it("returns employee ID", () => {
+        it("should return employee ID", () => {
             const employee = new Employee("Savannah", 1012341, "savannah@greenleaf.com");
             expect(employee.getId().toBe(1012341));
         });
     });
 
     describe("getEmail method", () => {
-        it("returns employee email", () => {
+        it("should return employee email", () => {
             const employee = new Employee("Flora", 1001126, "flora@greenleaf.com");
             expect(employee.getEmail().toBe("flora@greenleaf.com"));
         });
     });
 
     describe("getRole method", () => {
-        it("returns employee role", () => {
+        it("should return employee role", () => {
             const employee = new Employee("Bruce", 1023314, "bruce@greenleaf.com");
             expect(employee.getRole().toBe("Employee"));
         });
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -3,7 +3,7 @@ const Manager = require("../lib/Manager");
 
 describe("Manager class", () => {
     describe("getRole method", () => {
-        it("returns employee role", () => {
+        it("should return employee role", () => {
             const manager = new Manager("Barb", 1001128, "barb@greenleaf.com", "1026")
             expect(manager.getRole().toBe("Manager"));
         });
