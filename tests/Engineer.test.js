54547ea78c42d540b54875bbd27c6ace5a622980
coderbennett <joeykb17@gmail.com>
2022-04-23 15:33:39 -0700

    add test cases for engineer class

---
+++ tests/Engineer.test.js
@@ -0,0 +1,19 @@
+const Employee = require("../lib/Employee");
+const Engineer = require("../lib/Engineer");
+
+describe("Engineer class", () => {
+
+    describe("getGithub method", () => {
+        it("should return the engineer's github username", () => {
+            const engineer = new Engineer("Beatrice", 1012395, "bea@greenleaf.com", "bealanore");
+            expect(engineer.getGithub().toBe("bealanore"));
+        })
+    });
+
+    describe("getRole method", () => {
+        it("should return the employee's role", () => {
+            const engineer = new Engineer("Beatrice", 1012395, "bea@greenleaf.com", "bealanore");
+            expect(engineer.getRole().toBe("Engineer"));
+        })
+    });
+});
