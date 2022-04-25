d2f24cc7ccf2f9b9f5936a787a6dff742d1663e2
coderbennett <joeykb17@gmail.com>
2022-04-25 16:51:14 -0700

    add employee method test cases for engineer class

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -15,4 +15,25 @@ describe("Engineer class", () => {
             expect(engineer.getRole()).toBe("Engineer");
         })
     });
+
+    describe("getName method", () => {
+        it("should return employee id", () => {
+            const engineer = new Engineer("Kyle", 1014421, "kyle@greenleaf.com", "whilekyle");
+            expect(engineer.getName()).toBe("Kyle");
+        })
+    });
+
+    describe("getId method", () => {
+        it("should return employee id", () => {
+            const engineer = new  Engineer("Trevor", 1091223, "trevor@greenleaf.com", "trevorforever");
+            expect(engineer.getId()).toBe(1091223);
+        })
+    });
+
+    describe("getEmail method", () => {
+        it("should return employee email", () => {
+            const engineer = new Engineer("Helena", 1421412, "helena@greenleaf.com", "helenahhh");
+            expect(engineer.getEmail()).toBe("helena@greenleaf.com");
+        })
+    });
 });
