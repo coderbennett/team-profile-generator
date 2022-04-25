fad327aee439e00002bee68795c3248e1f788715
coderbennett <joeykb17@gmail.com>
2022-04-25 09:40:56 -0700

    adjust line indentation in generation js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -5,25 +5,25 @@ class generateHTML {
 
     generateContent() {
         return `<!DOCTYPE html>
-        <html data-theme="corporate" lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link href="https://cdn.jsdelivr.net/npm/daisyui@2.14.2/dist/full.css" rel="stylesheet" type="text/css" />
-            <title>Team Profile</title>
-        </head>
-        <body>
-            <div class="navbar bg-primary">
-                <a class="btn btn-ghost normal-case text-xl text-base-100">Our Team</a>
-            </div>
-            <div id="cards" class="flex flex-wrap justify-evenly">
-                ${this.iterateTeam(this.teamArray)}
-            </div>
-            <script src="https://cdn.tailwindcss.com"></script>
-            <script src="https://kit.fontawesome.com/afdfd2c8fa.js" crossorigin="anonymous"></script>
-        </body>
-        </html>`
+<html data-theme="corporate" lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/daisyui@2.14.2/dist/full.css" rel="stylesheet" type="text/css" />
+        <title>Team Profile</title>
+    </head>
+<body>
+    <div class="navbar bg-primary">
+        <a class="btn btn-ghost normal-case text-xl text-base-100">Our Team</a>
+    </div>
+    <div id="cards" class="flex flex-wrap justify-evenly">
+        ${this.iterateTeam(this.teamArray)}
+    </div>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/afdfd2c8fa.js" crossorigin="anonymous"></script>
+</body>
+</html>`
     }
 
     iterateTeam() {
@@ -50,17 +50,17 @@ class generateHTML {
     
         return `
     
-    <div class="card w-96 bg-secondary shadow-xl m-4">
-        <div class="bg-primary">
-            <h2 class="card-title m-3 text-base-100">${employee.name}</h2>
-            <h2 class="card-title m-3 text-base-100">${type}</h2>
+        <div class="card w-96 bg-secondary shadow-xl m-4">
+            <div class="bg-primary">
+                <h2 class="card-title m-3 text-base-100">${employee.name}</h2>
+                <h2 class="card-title m-3 text-base-100">${type}</h2>
+            </div>
+            <div class="card-body">
+                <p class="bg-base-100 rounded p-2">ID: ${employee.id}</p>
+                <p class="bg-base-100 rounded p-2">Email: <a href='mailto:${employee.email}'>${employee.email}</a></p>
+                <p class="bg-base-100 rounded p-2">${property}</p>
+            </div>
         </div>
-        <div class="card-body">
-            <p class="bg-base-100 rounded p-2">ID: ${employee.id}</p>
-            <p class="bg-base-100 rounded p-2">Email: <a href='mailto:${employee.email}'>${employee.email}</a></p>
-            <p class="bg-base-100 rounded p-2">${property}</p>
-        </div>
-    </div>
         `
     }
 }
