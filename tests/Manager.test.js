0c89ae6a2cc9ceeec49362517be9eca0cbefb6eb
coderbennett <joeykb17@gmail.com>
2022-04-25 16:36:52 -0700

    add employee method tests to manager tests

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -3,8 +3,22 @@ const Manager = require("../lib/Manager");
 describe("Manager class", () => {
     describe("getRole method", () => {
         it("should return employee role", () => {
-            const manager = new Manager("Barb", 1001128, "barb@greenleaf.com", "1026")
+            const manager = new Manager("Barb", 1001128, "barb@greenleaf.com", "1026");
             expect(manager.getRole()).toBe("Manager");
         });
+    });
+
+    describe("getId method", () => {
+        it("should return employee id", () => {
+            const manager = new Manager("Theodosia", 1002402, "theodosia@greenleaf.com", "1231");
+            expect(manager.getId()).toBe(1002402);
+        })
+    });
+
+    describe("getEmail method", () => {
+        it("should return employee email", () => {
+            const manager = new Manager("Ysabelle", 1013401, "ysabelle@greenleaf.com", "1023");
+            expect(manager.getEmail()).toBe("ysabelle@greenleaf.com");
+        })
     });
 });
